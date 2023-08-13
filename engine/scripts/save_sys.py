240ce95fa8299cfcf0eeab40fb8b9fbd6fe45507
oledjigames <61412509+oledjigames@users.noreply.github.com>
2023-08-13 05:47:28 +0300

    Add files via upload
    fixed crash, save characters on screen not working, soon it will be fixed

--- engine/scripts/save_sys.py
+++ engine/scripts/save_sys.py
@@ -2,8 +2,8 @@
 def quick_save():
 	global id_current, choice_var, background_screen, character, background_id
 	savefile = open('savegame/savefile.aves','w+')
-	my_string = ''.join(''.join(l) for l in character)
-	savefile.write("global id_current, choice_var, background_screen, character, background_id\nid_current="+str(id_current)+"\nchoice_var="+str(choice_var)+"\nbackground_id="+str(background_id)+"\ncharacter="+str(my_string)+"\nbackground_screen  = background[background_id]")
+	#my_string = ''.join(''.join(l) for l in character)
+	savefile.write("global id_current, choice_var, background_screen, character, background_id\nid_current="+str(id_current)+"\nchoice_var="+str(choice_var)+"\nbackground_id="+str(background_id)+"\nbackground_screen  = background[background_id]")
 	savefile.close()
 def quick_load():
 	global id_current, choice_var, background_screen, character, background_id
